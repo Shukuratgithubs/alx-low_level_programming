9ed32037c27b0986c3d5252a37516f7310f6e8fa
shukuratgithubs <shukroh74@gmail.com>
2022-04-18 14:52:07 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,9 +8,9 @@
 int main(void)
 {
         int i;
-        four (i = 48; i < 58; i++)
+        for (i = 48; i < 58; i++)
         {
-                putchar(i);
+        putchar(i);
         }
         putchar('\n');
         return (0);
