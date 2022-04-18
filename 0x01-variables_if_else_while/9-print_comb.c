ba5d3351f2ba4e67807c661a6c301c12ef340562
shukuratgithubs <shukroh74@gmail.com>
2022-04-18 15:31:32 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - entry point
  *
@@ -7,15 +8,16 @@
  */
 int main(void)
 {
-int i;
-for (i = 10; i < 20; i++)
-{
-putchar((i % 10) + '0');
-if (i != 19)
-{
-putchar(',');
-putchar(' ');
-}
-putchar('\n');
-return (0);               
+        int i;
+        for (i = 10; i < 20; i++)
+        {
+                putchar((i % 10) + '0');
+                if (i != 19)
+                {
+                putchar(',');
+                putchar(' ');
+                }
+        }
+        putchar('\n');
+        return (0);
 }
