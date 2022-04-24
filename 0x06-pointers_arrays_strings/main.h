d6b7f796ae9e7760b7c1daeec2c97c08b51667ca
shukuratgithubs <shukroh74@gmail.com>
2022-04-24 13:47:15 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,27 +1,16 @@
-#include "main.h"
-
-/**
- * _strcat - concatinate 2 strings.
- * @dest: first string.
- * @src: second string.
- * Return: string.
- */
-
-char *_strcat(char *dest, char *src)
-{
-	int i = 0, j = 0;
-
-	while (dest[i] != '\0')
-		i++;
-
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-
-	dest[i] = '\0';
-
-	return (dest);
-}
+#ifndef MAIN_H
+#define MAIN_H
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *str);
+char *cap_string(char *str);
+char *leet(char *str);
+char *rot13(char *str);
+void print_number(int n);
+int _putchar(char c);
+void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+#endif /*MAIN_H*/
