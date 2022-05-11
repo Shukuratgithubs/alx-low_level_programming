1c36d0fbc7241fead72126fb9f5e5a608635d88c
shukuratgithubs <shukroh74@gmail.com>
2022-05-11 12:26:27 -0700

    main.h

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -1,5 +1,12 @@
 #ifndef DOG
 #define DOG
+
+/**strict dog - dog basic info
+* @name: first member
+*@age: second member
+*@owner: third member
+*Description: a longer description
+*/
 struct dog
 {
 	char *name;
